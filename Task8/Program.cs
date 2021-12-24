bd0472013aeb9cdad6b0bcafbcb8a0f263e17c23
Mirlan <mirlandaniaruulu19@gmail.com>
2021-12-24 22:48:06 +0600

    Add project files.

---
+++ Task8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary m = new MyDictionary();
+            m["one"] = 1;
+            m["two"] = 2;
+            m["three"] = 3;
+            m["ten"] = 10;
+            Console.WriteLine(m["ten"]);
+        }
+    }
+
+    public class MyDictionary
+    {
+        private Dictionary<string, int> a = new Dictionary<string, int>();
+
+        public int this[string s]
+        {
+            get
+            {
+                return a[s];
+            }
+            set
+            {
+                a[s] = value;
+            }
+        }
+    }
+}
